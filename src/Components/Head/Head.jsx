e65dae456ab1ebbe796f01cb81fd700f2f7dc26f
JuanaGonzalez21 <juanita.juana@hotmail.com>
2024-04-18 21:25:32 -0600

    page and assets

---
+++ src/Components/Head/Head.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Head.css'
+import icon from '../../assets/logo.svg'
+const Head = () => {
+  return (
+    <div className='head'>
+        <img src={icon} alt="" />
+        <div className="head_background">
+            <h1>Body Mass Index Calculator</h1>
+            <p>Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being. </p>
+        </div>
+    </div>
+  )
+}
+
+export default Head
